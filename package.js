e86b05a8e7535d0b04d76a4b979a021aab102728
Kim DoJin <jakekim84@gmail.com>
2022-05-04 16:29:26 +0900

    Update package.js

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
   "version": "0.0.1",
   "description": "A simple HTTP hello world server for testing.",
   "main": "index.js",
-  "repository": "https://github.com/masterful/nodejs-hello-world",
-  "author": "Pim Vermeyden <pim2345@gmail.com>",
+  "repository": "https://github.com/bresting/nodejs-hello-world",
+  "author": "jakekim84 <jakekim84@gmail.com>",
   "license": "MIT"
 }
